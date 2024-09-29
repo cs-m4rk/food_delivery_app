3361b3c7bfa24096d086b0ca88e1e06c4d10f451
Zai <cortezvince998@gmail.com>
2024-09-29 17:56:04 +0800

    profile screen
    Profile screen layout, only edit profile button is working and edit profile not yet functional

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -2,13 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:food_delivery_app/components/bottom_navbar.dart';
 import 'package:food_delivery_app/screens/food_screen.dart';
 import 'package:food_delivery_app/screens/login_screen.dart';
+import 'package:food_delivery_app/screens/profile_screen.dart';
 import 'package:food_delivery_app/screens/register_screen.dart';
+import 'package:food_delivery_app/screens/update_profile_screen.dart';
 
 class AppRoutes {
   static const String login = '/';
   static const String register = '/register';
   static const String home = '/home';
   static const String food = '/food';
+  static const String updateProfile = '/updateProfile';
+    static const String profile = '/profile';
 
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
@@ -20,6 +24,8 @@ class AppRoutes {
         return MaterialPageRoute(builder: (_) => FoodScreen());
       case register:
         return MaterialPageRoute(builder: (_) => RegisterScreen());
+      case updateProfile:
+        return MaterialPageRoute(builder: (_) => UpdateProfileScreen());
       default:
         return MaterialPageRoute(
           builder: (_) => Scaffold(
